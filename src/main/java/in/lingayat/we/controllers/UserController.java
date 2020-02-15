1a2d102a2f47a980f560bff7d144bff730e0e3b8
maniyalraj <maniyalraj@gmail.com>
2020-02-15 14:51:25 +0530

    Comment dedicated port from props

---
+++ src/main/java/in/lingayat/we/controllers/UserController.java
@@ -0,0 +1,26 @@
+package in.lingayat.we.controllers;
+
+import in.lingayat.we.models.CurrentUser;
+import in.lingayat.we.models.UserPrincipal;
+import in.lingayat.we.payload.UserSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+}
